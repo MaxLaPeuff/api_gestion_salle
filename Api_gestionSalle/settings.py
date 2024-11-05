dd744bab30f960a6c71b7cbde133995ffcca39fc
MaxLaPeuff <maxaraye18@gmail.com>
2024-11-05 08:21:34 +0100

    correction structure projet

--- Api_gestionSalle/settings.py
+++ Api_gestionSalle/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-hc-+w6yis$#0$rb#z53zio
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG','True')=="True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','api-gestion-salle.onrender.com']
 
 
 # Application definition
